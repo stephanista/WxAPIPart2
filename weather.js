6c3735bcf53a590fe340321d866d68beb282c690
stephanista <houser16@uw.edu>
2017-07-19 20:06:03 -0700

    Everything working nicely except 401 error on the openwx xmlhttprequest. when I use the created link though it works perfectly

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const apiURL = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather";
-const appID = "a4192ac11c66b463d93c577e64fda186"; //yes thats mine
+const appID = "&units=metric&APPIDa4192ac11c66b463d93c577e64fda186"; //yes thats mine
 let debug = null
 const googKey = "AIzaSyCtTvmh8I62BBrU7qOMjAvvsfBLifI2KxI";
 
@@ -8,7 +8,7 @@ londonlink = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/
 
 seattlelink = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=47.6762&lon=-122.3182&units=imperial&appid=a4192ac11c66b463d93c577e64fda186"
 
-// userlink = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?XXXXXXXXXunits=imperial&appid=a4192ac11c66b463d93c577e64fda186"
+// userlink = https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=47.3588&lon=-122.0442&units=metric&appid=a4192ac11c66b463d93c577e64fda186
 
 
 function getSeaWx() {
@@ -57,68 +57,38 @@ function getSeaWx() {
     request.send()
     }
 
-//userloc
+//userloc thanks to watching Cheri multiple times
 
-function wow(){
-  // get user's location from the browser
-  navigator.geolocation.getCurrentPosition(geolocSuccess, geolocError);
-
-  // callback for successfully getting location from user's browser
-  function geolocSuccess(position){
-    const newPos = {lat: position.coords.latitude, lng: position.coords.longitude};
-    getLocation(newPos);
-  }
-
-  // callback for no success getting location from user's browser
-  function geolocError(){
-    console.log("Error getting user's location :(");
-  }
-
-  // API call onload callback function
-  function onloadFunc(){
-    const resp = JSON.parse(this.response);
-    console.log(resp);
-
-  }
-
-  // API call onerror callback function
-  function onerrorFunc(){
-    // print an error message to page
-     console.log("Sorry, an error occurred");
-  }
-
-  // helper method to call API and convert longitude & latitude to a human friendly address
-  function getLocation(locObj){
-    let mapUri = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${locObj.lat},${locObj.lng}&key=AIzaSyCtTvmh8I62BBrU7qOMjAvvsfBLifI2KxI`;
-    // new request object- open, define callbacks, send
-    let request = new XMLHttpRequest();
-    request.open("GET", mapUri, true);
-    request.onload = onloadFunc;
-    request.onerror = onerrorFunc;
-    request.send();
+function getUserWeather(){//Onclick button for User's City
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(userWeatherapi);
   }
 }
 
-    // function getUserWx() {
-    //
-    //   var request = new XMLHttpRequest()
-    //   request.open("GET", userlink, true)
-    //   request.onload = function () {
-    //     let wxNameDiv = document.getElementById("displaywxname")
-    //     let wxGenDiv = document.getElementById("displaywxgen")
-    //     let wxTempDiv = document.getElementById("displaywxtemp")
-    //     let wxHumDiv = document.getElementById("displaywxhum")
-    //     let wxWindsDiv = document.getElementById("displaywxwinds")
-    //     let response = JSON.parse(request.response)
-    //     wxNameDiv.innerHTML = ("Station location: " + response.name)
-    //     wxGenDiv.innerHTML = (response.weather[0].description.charAt(0).toUpperCase() + response.weather[0].description.slice(1))
-    //     wxTempDiv.innerHTML = (response.main.temp + " degrees Fahrenheit")
-    //     wxHumDiv.innerHTML = (response.main.humidity + "% humidity")
-    //     wxWindsDiv.innerHTML = ("Winds " + response.wind.speed + " at " + response.wind.deg)
-    //   }
-    //   request.error = function (errorObject) {
-    //     console.log("Oops.. You bwoke it.")
-    //     console.log(errorObject)
-    //   }
-    //   request.send()
-    //   }
+function userWeatherapi (position) {//OpenWeather Positioning
+  userPos = `?lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+  UserWeather(apiURL + userPos + appID);
+}
+
+function UserWeather(url) {//User Weather API response
+  let request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.onload = function (){
+    let wxNameDiv = document.getElementById("displaywxname")
+    let wxGenDiv = document.getElementById("displaywxgen")
+    let wxTempDiv = document.getElementById("displaywxtemp")
+    let wxHumDiv = document.getElementById("displaywxhum")
+    let wxWindsDiv = document.getElementById("displaywxwinds")
+    let response = JSON.parse(request.response)
+    wxNameDiv.innerHTML = ("Station location: " + response.name)
+    wxGenDiv.innerHTML = (response.weather[0].description.charAt(0).toUpperCase() + response.weather[0].description.slice(1))
+    wxTempDiv.innerHTML = (response.main.temp + " degrees Fahrenheit")
+    wxHumDiv.innerHTML = (response.main.humidity + "% humidity")
+    wxWindsDiv.innerHTML = ("Winds " + response.wind.speed + " at " + response.wind.deg)
+  }
+  request.error = function (errorObject) {
+    console.log("Oops.. You bwoke it.")
+    console.log(errorObject)
+  }
+  request.send()
+  }
